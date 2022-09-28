603cb7b1a556570ffc45a80b0ea857a200eec2b7
Charles Thomas <charlesthomas@mbp-de-charles.home>
2022-09-28 14:57:31 +0200

    Adding files

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -1,5 +1,6 @@
 import { Skill } from "../typepings";
 
+
 export const fetchSkills = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
     const data = await res.json();
